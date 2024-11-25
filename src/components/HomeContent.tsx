280217554b2c2ec2362255415b7132d82e378612
Ndoll7 <164797060+Ndoll7@users.noreply.github.com>
2024-11-25 05:50:23 -0800

    Add files via upload

---
+++ src/components/HomeContent.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomeContent = () => {
+  return (
+    <div>
+      <section className='hero' style={{backgroundImage: "url(backgroung-pic1.png)"}}>
+      <div>
+        <h1 className='fade-in'>Welcome to Watch World Cars</h1>
+        <p>Your one-stop shop for the best cars</p>
+        <button>Shop Now</button>
+      </div>
+      </section>
+      
+    </div>
+  )
+}
+
+export default HomeContent
